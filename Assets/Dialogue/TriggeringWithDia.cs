9f16a44f0d804531249ac76fdab72383e8275dce
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-07-17 15:37:33 +0900

    1
    1

--- Assets/Dialogue/TriggeringWithDia.cs
+++ Assets/Dialogue/TriggeringWithDia.cs
@@ -7,12 +7,14 @@ using UnityEngine;
 public class TriggeringWithDia : MonoBehaviour
 {
     [SerializeField] private string diatext;
+    [SerializeField] private Color color;
+    [SerializeField] private float plainTime = 1f;
 
-    private void OnTriggerEnter2D(Collider2D other)
+        private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            DialogueManager.GetInstance().SetUpDialogue(diatext,transform.position,Color.white);
+            DialogueManager.GetInstance().SetUpDialogue(diatext,transform.position,color,plainTime);
         }
     }
 }
