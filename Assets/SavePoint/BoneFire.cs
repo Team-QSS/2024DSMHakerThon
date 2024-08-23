4466d6a0d9a4b2aec1ba3d76b764e303eafa7231
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-08-23 16:50:41 +0900

    EndedBoneFire연출

--- Assets/SavePoint/BoneFire.cs
+++ Assets/SavePoint/BoneFire.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using player.script;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace SavePoint
 {
     public class BoneFire : MonoBehaviour
     {
-        private static Animator ani;
+        private static Animator _ani;
+        private static Light2D _light2D;
+        private static ParticleSystem _prt;
+        
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-            ani = GetComponent<Animator>();
+            _light2D = GetComponentInChildren<Light2D>();
+            _prt = GetComponentInChildren<ParticleSystem>();
+            _ani = GetComponent<Animator>();
+            _light2D.enabled = false;
+            _prt.Stop();
         }
         
 
@@ -19,8 +27,11 @@ namespace SavePoint
         {
             PlayerInteraction.isInteracting = true;
             Debug.Log("lit");
-            yield return new WaitForSeconds(sec);
-            ani.SetBool("lit",true);
+            yield return new WaitForSeconds(1f);
+            _prt.Play();
+            yield return new WaitForSeconds(1.2f);
+            _light2D.enabled = true;
+            _ani.SetBool("lit",true);
             PlayerInteraction.isInteracting = false;
 
         }
