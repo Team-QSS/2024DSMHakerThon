77287d132c1bbb8ccb0fa7b73709056bf16b2713
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-09-05 22:56:18 +0900

    DoneDoneWellDone

--- Assets/dummy/NextScene5.cs
+++ Assets/dummy/NextScene5.cs
@@ -3,9 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class NextScene5 : MonoBehaviour
 {
-    private void Start()
+
+    public void ToTitle()
     {
-        Time.timeScale = 1;
-        SceneManager.LoadScene("CrowChacing");
+        SceneManager.LoadScene("TitleScene");
     }
 }
